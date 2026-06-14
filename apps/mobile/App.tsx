ed2f08fa930a6653482593b7002dd830a8235d21
azka-apps <azkabashir402@gmail.com>
2026-06-14 09:40:44 +0400

    mobile code pushed

---
+++ apps/mobile/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <Text>Hello, React Native!</Text>
+    </SafeAreaProvider>
+  );
+}
+
+export default App;
